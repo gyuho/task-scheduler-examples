97b9702f195ccf5357131c95f1675355a4416bbc
Gyuho Lee <leegyuho@amazon.com>
2021-06-30 14:23:13 -0700

    project.sh: update
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- task-scheduler-rust/project.sh
+++ task-scheduler-rust/project.sh
@@ -5,29 +5,29 @@ cd ./task-scheduler-rust
 
 cargo install cargo-edit
 
-cd ./src
-touch lib.rs
+# create "lib.rs" to link modules
+touch ./src/lib.rs
 
 # "id.rs"
-touch id.rs
+touch ./src/id.rs
 cargo add rand
 
 # "notify.rs"
-touch notify.rs
+touch ./src/notify.rs
 cargo add uuid --features v4
 cargo add tokio --features full
 
 # "echo.rs"
-touch echo.rs
+touch ./src/echo.rs
 cargo add serde --features derive
 cargo add serde_json
 
 # "apply.rs"
-touch apply.rs
+touch ./src/apply.rs
 # none
 
 # "server.rs"
-touch server.rs
+touch ./src/server.rs
 cargo add http
 cargo add futures
 cargo add hyper --features full
