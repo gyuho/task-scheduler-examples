4c3eed22ddaa489878eaf7f6aef0b7d56800b74a
Gyuho Lee <leegyuho@amazon.com>
2021-06-30 16:03:55 -0700

    main.rs: update
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- task-scheduler-rust/src/main.rs
+++ task-scheduler-rust/src/main.rs
@@ -24,11 +24,12 @@ fn main() {
     let future_task = srv.start();
     let rt = tokio::runtime::Runtime::new().unwrap();
 
+    // we can run tokio within async_std and the other way around
+    // NOTE: DO NOT CALL THIS RECURSIVELY
+    // e.g. "Cannot start a runtime from within a runtime. This happens because a function (like `block_on`) attempted to block the current thread while the thread is being used to drive asynchronous tasks"
     let ret = rt.block_on(future_task);
     match ret {
         Ok(_) => println!("server done"),
-        Err(e) => {
-            println!("server aborted {}", e);
-        }
+        Err(e) => println!("server aborted {}", e),
     };
 }
